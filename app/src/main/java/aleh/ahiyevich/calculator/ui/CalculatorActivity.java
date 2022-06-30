c8c0fe8c707e832c22f6ce614230574523c42bd5
Aleh <aleh2806@yandex.ru>
2022-06-30 19:48:19 +0300

    Initial commit

---
+++ app/src/main/java/aleh/ahiyevich/calculator/ui/CalculatorActivity.java
@@ -0,0 +1,18 @@
+package aleh.ahiyevich.calculator.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import aleh.ahiyevich.calculator.R;
+
+public class CalculatorActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+
+    }
+}
