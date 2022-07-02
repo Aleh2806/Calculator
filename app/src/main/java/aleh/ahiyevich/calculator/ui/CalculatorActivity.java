b5a0e6d0132603f20a566f2cf98f2aa7106442c3
Aleh <aleh2806@yandex.ru>
2022-07-02 18:42:49 +0300

    Home_work_2

--- app/src/main/java/aleh/ahiyevich/calculator/ui/CalculatorActivity.java
+++ app/src/main/java/aleh/ahiyevich/calculator/ui/CalculatorActivity.java
@@ -3,16 +3,23 @@ package aleh.ahiyevich.calculator.ui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import aleh.ahiyevich.calculator.R;
 
-public class CalculatorActivity extends AppCompatActivity {
+public class CalculatorActivity extends AppCompatActivity{
+
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
-
     }
+
 }
